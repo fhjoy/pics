42df1543f852b338e6544ccb67c067a571ab5006
Md. Faisal Hossain <82816011+fhjoy@users.noreply.github.com>
2021-09-17 11:06:23 +0200

    removing comment

--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import React from "react";
 import reactDom from "react-dom";
 import App from "./components/App";
 
-// class App extends React.Component{
-//     render(){
-//         return(
-
-//         )
-//     }
-// }
 
 reactDom.render(<App />, document.querySelector("#root"));
